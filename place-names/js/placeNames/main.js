4ee4e2e77f04e13820a120ce3359e62b4fa02db5
Alex Prieu <daprieu@gmail.com>
2021-11-03 10:02:43 -0500

    first commit

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -5,8 +5,25 @@ const names = [
     "London", "The mall", "Ryman Auditorium", "The Great Wall of China"
 ];
 
+// Print some output to the terminal (aka. "Console");
 console.log("All Pace Names");
 
+// Iterate over each "name" in the "names" list
 for (const name of names) {
   console.log(name);
+}
+
+// When you don't pass an argument to WriteLine, it simply outputs a blank line.
+//  Here we use that to add a little space around the output.
+console.log();
+
+// Filter the list to create a new list that only has 
+//  place names that start with the word "The".
+
+const theNames = names.filter(n => n.startsWith("The")) 
+
+console.log("'The' Place Names");
+for (const name of theNames)
+{
+    console.log(name);
 }
